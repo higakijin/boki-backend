20c8a29f0b30bc23a2805338a2264198347bbdaf
higakijin <hm385.chejptks@gmail.com>
2022-03-01 00:15:12 +0900

    pry-railsの導入とusersコントローラーの作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+
+  def check_current_user
+    if current_user
+      render json: { data: current_user }
+    else
+      render json: { data: '' }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'auth/registrations'
   }
+  get 'currentUser' => 'users#check_current_user'
 end
