9e820fd0b7375dcb123bb5cd9566043ca82b31d4
higakijin <hm385.chejptks@gmail.com>
2022-03-07 01:51:31 +0900

    outputのuserにセリアライザを適用

--- app/serializers/output_serializer.rb
+++ app/serializers/output_serializer.rb
@@ -1,3 +1,4 @@
 class OutputSerializer < ActiveModel::Serializer
-  attributes %i[id post created_at updated_at user lesson]
+  attributes %i[id post created_at updated_at lesson]
+  belongs_to :user, serializer: UserSerializer 
 end
