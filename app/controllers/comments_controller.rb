6d6a4f917b1dcc3f22dd46a4815d183e51b7d435
higakijin <hm385.chejptks@gmail.com>
2022-03-08 19:38:52 +0900

    admin::usersの作成など

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,7 @@ class CommentsController < ApplicationController
     comment = current_user.comments.new(comment_params)
     comment.output_id = params[:output_id].to_i
     output = Output.find(params[:output_id])
-    if comment.save!
-      render json: output.comments
-    end
+    render json: output.comments if comment.save!
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  namespace :admin do
+    resources :users
+  end
+
   root 'users#check_current_user'
 end
